4ef9b9a7c228e95fa879478291947c7ec8f4b494
FilipKalinowski <251165@edu.p.lodz.pl>
2023-12-17 17:01:49 +0100

    01 zmiana metoda policz

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
+using namespace std;
+
+int Policz(int a, int b){
+    return a+b;
+}
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << std::endl;
+    cout << "W pierwszym branchu; 2+3=" << Policz(2,3) << std::endl;
+
+
     return 0;
 }
